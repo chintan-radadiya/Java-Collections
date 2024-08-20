82a7d00a6a262fdab142a0113320b72a1d9dc82f
chintan-radadiya <chintan96radadiya@gmail.com>
2024-08-21 02:08:28 +0530

    First Commit

---
+++ src/ArrayList/Print_using_Iteratos.java
@@ -0,0 +1,23 @@
+package ArrayList;
+import java.util.*;
+
+public class Print_using_Iteratos
+{
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+    Scanner sc =  new Scanner(System.in);
+
+        for (int i = 1; i <=10; i++) {
+            list.add(i*10);
+        }
+
+        System.out.println(list);
+
+        Iterator<Integer> itr = list.iterator();
+
+        while(itr.hasNext()){
+            System.out.print(itr.next() + "\t");
+        }
+
+    }
+}
