58b0ef805d4183f51ec09ad523fe87de56cba4d5
chintan-radadiya <chintan96radadiya@gmail.com>
2024-08-22 22:08:09 +0530

    First Commit

---
+++ src/Employee/Employee.java
@@ -0,0 +1,14 @@
+package Employee;
+
+public class Employee {
+
+    int id;
+    String name;
+    int salary;
+
+    Employee(int id,String name, int salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+}
