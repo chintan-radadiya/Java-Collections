ffa6cf96494b5070782065823b9bfacbb55ff2f1
chintan-radadiya <chintan96radadiya@gmail.com>
2024-08-29 16:21:50 +0530

    First commit

--- src/POJO/FullDetailsArray/Main.java
+++ src/POJO/FullDetailsArray/Main.java
@@ -1,11 +1,15 @@
 package POJO.FullDetailsArray;
 
+
 import java.util.*;
 import java.util.ArrayList;
 
 public class Main {
 
     public static	void main(String[] args) {
+
+        Scanner sc =new Scanner(System.in);
+
         int[] ids = {101, 102, 103, 104, 105, 106};
         String[]	names	=	{"Amar",	"Hareen",	"Sathya",	"Annie",	"Raji",	"Harsha"};
         int[] salaries = {30000, 35000, 40000, 45000, 42000, 50000};
@@ -26,14 +30,85 @@ public class Main {
             list.add(e);
         }
 
+    /*
         for(Employee e: list){
             System.out.println("Employee Id: " + e.getId());
             System.out.println("Employee Name: " + e.getName());
             System.out.println("Employee Salary: " + e.getSalary());
             System.out.println("Employee Department: " + e.getDept());
             System.out.println("Employee City: " + e.getCity());
-        System.out.println("-------------------------");
+            System.out.println("-------------------------");
+        }
+    */
+
+//        Details using for loop in reverse order.
+   /*     for(int i = list.size()-1; i>=0; i--){
+
+            Employee e = list.get(i);
+
+            System.out.println(e.getId());
+            System.out.println(e.getName());
+            System.out.println(e.getSalary());
+            System.out.println(e.getDept());
+            System.out.println(e.getCity());
+            System.out.println("=====================");
         }
 
+    */
+
+//        Display employee where ID is xyz.
+   /*     System.out.println("Enter emp id to find the employee details:");
+        int choice = sc.nextInt();
+
+        for(Employee e : list){
+            if(choice == e.getId()){
+
+                System.out.println(e.getId());
+                System.out.println(e.getName());
+                System.out.println(e.getSalary());
+                System.out.println(e.getDept());
+                System.out.println(e.getCity());
+                break;
+            }
+        }
+*/
+
+//        List employees from specific city.
+    /*    System.out.println("Enter city name to print employee details:");
+        String choice = sc.next();
+        int count =0;
+
+        for(Employee e: list) {
+            if(e.getCity().equals(choice)){
+                System.out.println(e.getId());
+                System.out.println(e.getName());
+                System.out.println(e.getSalary());
+                System.out.println(e.getDept());
+                System.out.println(e.getCity());
+                count++;
+            }
+        }
+        if(count == 0){
+            System.out.println("City not Found !!");
+        }
+    */
+
+//    Count of how mny employee working in hederabad.
+    /*    int count =0;
+
+        for(Employee e: list) {
+            if(e.getCity().equals("Hyderabad") || e.getCity().equals("Chennai")){
+                System.out.println(e.getId());
+                System.out.println(e.getName());
+                System.out.println(e.getSalary());
+                System.out.println(e.getDept());
+                System.out.println(e.getCity());
+                System.out.println();
+                count++;
+            }
+        }
+
+        System.out.print("Employee count wirking in hyderabad and chennai is:" + count);
+*/
 }
 }
